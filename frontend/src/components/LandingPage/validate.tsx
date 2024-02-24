7f4fe5304ef5468170bbc4c34fef0562c12483ff
aleev97 <valverdealejandro327@gmail.com>
2024-02-24 13:46:59 -0300

    refactorizacion y correccion

--- frontend/src/components/LandingPage/validate.tsx
+++ frontend/src/components/LandingPage/validate.tsx
@@ -1,17 +1,18 @@
 import { Errors } from "../../types";
 
-export default function validate({ username, password }: { username: string; password: string }): Errors {
+export default function validate({ username, email, password }: { username: string; email: string; password: string }): Errors {
   let errors: Errors = {};
 
   if (!username) {
     errors = { ...errors, username: "Debe ingresar un nombre de usuario" };
   } else if (username.length < 3 || username.length > 20) {
     errors = { ...errors, username: "El nombre de usuario debe tener entre 3 y 20 caracteres" };
-  } else if (!/^[a-zA-Z0-9_]+$/.test(username)) {
-    errors = { ...errors, username: "El nombre de usuario solo puede contener letras, números y guiones bajos" };
   }
-
-
+  if (!email) {
+    errors = { ...errors, email: "Debe ingresar un correo electrónico" };
+  } else if (!/\S+@\S+\.\S+/.test(email)) {
+    errors = { ...errors, email: "Debe ingresar un correo electrónico válido" };
+  }
 
   if (!password) {
     errors = { ...errors, password: "Debe ingresar una contraseña" };
